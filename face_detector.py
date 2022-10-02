5d0e8a1c0fd1803453072a731097803c8c3beba9
z1chh <lolchen2012@gmail.com>
2022-10-02 17:25:38 -0400

    Get coordinates of faces

--- face_detector.py
+++ face_detector.py
@@ -5,14 +5,18 @@ import cv2
 trained_face_data = cv2.CascadeClassifier(
     "haarcascade_frontalface_default.xml")
 
-# Choose an image to use to detect faces from
+# Choose an image to use to detect faces from, and convert it to grayscale
 img1 = cv2.imread("images/faces1.jpg")
+img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 
-# Display the image
-cv2.imshow("Face Detector", img1)
+# Detect faces
+face_coords = trained_face_data.detectMultiScale(img1)
+print(face_coords)
 
-# Wait for keyboard input before closing the image
-cv2.waitKey()
+# Display the images
+#cv2.imshow("Face Detector", img1)
+cv2.waitKey()  # Wait for keyboard input before closing the image
+
 
 # Confirm program is up and running
 print("Program successfully exited...")
