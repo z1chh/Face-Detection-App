c149855f24e4b114c429d7727dc27d002ab6583f
z1chh <lolchen2012@gmail.com>
2022-10-02 17:43:33 -0400

    Implemented basic face detector

--- face_detector.py
+++ face_detector.py
@@ -6,15 +6,20 @@ trained_face_data = cv2.CascadeClassifier(
     "haarcascade_frontalface_default.xml")
 
 # Choose an image to use to detect faces from, and convert it to grayscale
-img1 = cv2.imread("images/faces1.jpg")
-img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img = cv2.imread("images/faces2.jpg")
+grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Detect faces
-face_coords = trained_face_data.detectMultiScale(img1)
+face_coords = trained_face_data.detectMultiScale(grayscale_img)
+
+# Draw a rectangle around the face
 print(face_coords)
+for x1, y1, x2, y2 in face_coords:
+    #cv2.rectangle({IMAGE}, (x1, y1), (x2, y2), BGR(), THICKNESS)
+    cv2.rectangle(img, (x1, y1), (x1 + x2, y1 + y2), (0, 255, 0), 2)
 
 # Display the images
-#cv2.imshow("Face Detector", img1)
+cv2.imshow("Face Detector", img)
 cv2.waitKey()  # Wait for keyboard input before closing the image
 
 
