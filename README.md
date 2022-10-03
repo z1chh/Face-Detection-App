6c61015e1fba4c840a4c2a9a5f2888eb296a1aa4
z1chh <lolchen2012@gmail.com>
2022-10-02 23:51:43 -0400

    Added number of faces detected

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Face-Detection-App
+
+## face_finder.py
+
+Program that detects faces in a given image (change at line 7: image_to_use). Faces that are detected have a green rectangle around them.
+This program uses OpenCV's Haar Cascade algorithm.
+
+## face_detector.py
+
+Program that detects faces in real-time, for example with a live video. Faces that are detected have a green rectangle around them.
+This program uses OpenCV's Haar Cascade algorithm.
--- face_finder.py
+++ face_finder.py
@@ -1,14 +1,17 @@
 # Program to detect faces in images
-# Modify original image at line 11
+# Modify original image at line 7
 import cv2
 
 
-# Load pre-trained data on frontal faces from opencv (haar cascade algorithm)
+# Image to use
+image_to_use = "images/faces2.jpg"
+
+# Load pre-trained data on frontal faces from OpenCV (Haar Cascade algorithm)
 trained_face_data = cv2.CascadeClassifier(
-    "haarcascade_frontalface_default.xml")
+    "algos/haarcascade_frontalface_default.xml")
 
 # Choose an image to use to detect faces from, and convert it to grayscale
-img = cv2.imread("images/faces1.jpg")
+img = cv2.imread(image_to_use)
 grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Detect faces
@@ -20,9 +23,14 @@ for x, y, w, h in face_coords:
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 # Display the images
+num_faces = len(face_coords)
+print("{x} faces detected.".format(x=num_faces) if num_faces >
+      1 else "{x} face detected.".format(x=num_faces))
 img = cv2.resize(img, (960, 540))
 cv2.imshow("Face Detector", img)
-cv2.waitKey()  # Wait for keyboard input before closing the image
+
+# Wait for keyboard input before closing the image
+cv2.waitKey()
 
 
 # Confirm program is up and running
