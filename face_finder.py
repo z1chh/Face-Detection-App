8d1b2e820e207b36b5c958d2170b3fede927cbfc
z1chh <lolchen2012@gmail.com>
2022-10-02 23:41:27 -0400

    Finished program that detects faces in images

--- face_finder.py
+++ face_finder.py
@@ -1,3 +1,5 @@
+# Program to detect faces in images
+# Modify original image at line 11
 import cv2
 
 
@@ -12,10 +14,10 @@ grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detect faces
 face_coords = trained_face_data.detectMultiScale(grayscale_img)
 
-# Draw a rectangle around the face
-for x1, y1, x2, y2 in face_coords:
-    #cv2.rectangle({IMAGE}, (x1, y1), (x2, y2), BGR(), THICKNESS)
-    cv2.rectangle(img, (x1, y1), (x1 + x2, y1 + y2), (0, 255, 0), 2)
+# Draw a rectangle around each detected face
+for x, y, w, h in face_coords:
+    #cv2.rectangle({IMAGE}, (x, y), (width, heigth), BGR(), THICKNESS)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 # Display the images
 img = cv2.resize(img, (960, 540))
